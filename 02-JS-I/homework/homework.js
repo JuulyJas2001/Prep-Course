8218d4ecce18aa767b68902ad77a6af8fd2eeb48
Juulyjas2001 <juulyjas@gmail.com>
2022-08-05 12:45:45 -0300

    menos de noventa

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -82,7 +82,8 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num<90)
+  true
 }
 
 function mayorQueCincuenta(num) {
